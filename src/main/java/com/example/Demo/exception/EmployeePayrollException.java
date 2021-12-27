fbab6c3a2ecce919654ae0efae9758ba3ba14c3e
bharaths1513 <bharaths.1311@gmail.com>
2021-12-27 18:32:20 +0530

    added: Exception handler

---
+++ src/main/java/com/example/Demo/exception/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.example.Demo.exception;
+
+public class EmployeePayrollException extends RuntimeException {
+
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}
